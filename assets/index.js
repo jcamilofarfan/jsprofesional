eb0a71b3e00bb06ac14b2f7fa390c84eccd836b9
Juan Camilo Farfan <farfan1976@hotmail.com>
2020-12-22 07:34:36 -0500

    merge index

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,8 @@
-import mediaPLayer from ./MediaPLayer.js;
+import mediaPLayer from './MediaPLayer.js';
 import AutoPLay from './MediaPLayer.js';
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 
->>>>>>> 3b10c9e10938db0f1c990cd5907d7779c1f3cc68
 const player = new mediaPLayer({ el: video });
 button.onclick = () => player.play();
