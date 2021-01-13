bf59eb25cbcefbe86bfbc62f7902d4a11aacdaed
Juan Camilo Farfan <farfan1976@hotmail.com>
2021-01-13 12:14:34 -0500

    promises y proxys

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,15 @@
 import mediaPLayer from './MediaPLayer.js';
 import AutoPLay from './plugins/AutoPLay.js';
 
-const video = document.querySelector("video");
+const video = document.querySelector('video');
 const button = document.querySelector(".play");
 const buttonmute = document.querySelector(".muteunmute");
 
-const player = new mediaPLayer({ el: video, plugins:[
-    //new AutoPLay()
-] });
+const player = new mediaPLayer({
+    el: video,
+    plugins:[
+        new AutoPLay()
+    ],
+});
 button.onclick = () => player.togglePlay();
 buttonmute.onclick = () => player.toggleMute();
--- assets/plugins/AutoPLay.js
+++ assets/plugins/AutoPLay.js
@@ -1,6 +1,8 @@
 function AutoPLay() {}
 AutoPLay.prototype.run = function (player) {
-  player.mute();
+  if (!player.muted){
+    player.muted = true;
+  }
   player.play();
 };
 export default AutoPLay;
